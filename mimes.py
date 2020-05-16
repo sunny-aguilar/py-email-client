df58dfc89cb8c21fd6caaa3ddc0a526d0d4f027c
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-16 15:36:51 -0700

    email

---
+++ mimes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+#   This script implements a simple email client to prepare an email for
+#   sending. MIME extension is used for attachments.
+#
+#
+#   Author:     Sandro Aguilar
+#   Date:       May 16, 2020
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+import os.path, mimetypes
+
+attachment_path = 'banana.gif'
+attachment_filename = os.path.basename(attachment_path)
+
+mime_type, _ = mimetypes.guess_type(attachment_path)
+print(mime_type)
+
