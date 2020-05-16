a0616d82991171aeb8d060902fd317e5f5c0a54b
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-16 15:58:18 -0700

    email

--- email_template.py
+++ email_template.py
@@ -40,3 +40,12 @@ mime_type, mime_subtype = mime_type.split('/', 1)
 print(mime_type)
 print(mime_subtype)
 
+# add attachment
+with open(attachment_path, 'rb') as ap:
+    message.add_attachment(ap.read(),
+    maintype=mime_type,
+    subtype=mime_subtype,
+    filename=os.path.basename(attachment_path))
+
+
+
