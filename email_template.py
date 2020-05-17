8c2eca25b3cb14132df8c4d2b889e068449ae0c1
sunny-aguilar <sunny.aguilar@gmail.com>
2020-05-17 02:38:15 -0700

    runs

--- email_template.py
+++ email_template.py
@@ -8,8 +8,8 @@
 #   Author:     Sandro Aguilar
 #   Date:       May 16, 2020
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# import os.path, mimetypes, smtplib, getpass
-# from email.message import EmailMessage
+import os.path, mimetypes, smtplib, getpass
+from email.message import EmailMessage
 
 # message = EmailMessage()
 
@@ -28,71 +28,71 @@ body = """Hey there!
 message.set_content(body)
 
 
-# # MIME data
-# attachment_path = './banana.gif'
-# attachment_filename = os.path.basename(attachment_path)
+# MIME data
+attachment_path = './banana.gif'
+attachment_filename = os.path.basename(attachment_path)
 
-# mime_type, _ = mimetypes.guess_type(attachment_path)
-# print(mime_type)
+mime_type, _ = mimetypes.guess_type(attachment_path)
+print(mime_type)
 
-# mime_type, mime_subtype = mime_type.split('/', 1)
-# print(mime_type)
-# print(mime_subtype)
+mime_type, mime_subtype = mime_type.split('/', 1)
+print(mime_type)
+print(mime_subtype)
 
-# # add attachment
-# with open(attachment_path, 'rb') as ap:
-#     message.add_attachment(ap.read(),
-#     maintype=mime_type,
-#     subtype=mime_subtype,
-#     filename=os.path.basename(attachment_path))
-
-
-# # show message
-# # print(message)
+# add attachment
+with open(attachment_path, 'rb') as ap:
+    message.add_attachment(ap.read(),
+    maintype=mime_type,
+    subtype=mime_subtype,
+    filename=os.path.basename(attachment_path))
 
 
-# # create a mail server
-# mail_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-# mail_server.starttls()
-# mail_pass = getpass.getpass('Password?')
-# # print(mail_pass)
-
-# # login
-# mail_server.login(sender, mail_pass)
-
-# # send mail
-# mail_server.send_message(message)
-
-# # close connection
-# mail_server.quit()
+# show message
+# print(message)
 
 
-# import smtplib
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.text import MIMEText
-# mail_content = '''Hello,
-# This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library.
-# Thank You
-# '''
+# create a mail server
+mail_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+mail_server.starttls()
+mail_pass = getpass.getpass('Password?')
+# print(mail_pass)
 
-# #The mail addresses and password
-# sender_address = 'sunny.aguilar@gmail.com'
-# sender_pass = 'a1s2d3F$G%'
-# receiver_address = 'vluedevil@yahoo.com'
-# #Setup the MIME
-# message = MIMEMultipart()
-# message['From'] = sender_address
-# message['To'] = receiver_address
-# message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
-# #The body and the attachments for the mail
-# message.attach(MIMEText(mail_content, 'plain'))
-# #Create SMTP session for sending the mail
-# session = smtplib.SMTP('smtp.gmail.com', 465) #use gmail with port
-# session.starttls() #enable security
-# session.login(sender_address, sender_pass) #login with mail_id and password
-# text = message.as_string()
-# session.sendmail(sender_address, receiver_address, text)
-# session.quit()
+# login
+mail_server.login(sender, mail_pass)
+
+# send mail
+mail_server.send_message(message)
+
+# close connection
+mail_server.quit()
+
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+mail_content = '''Hello,
+This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library.
+Thank You
+'''
+
+#The mail addresses and password
+sender_address = 'sunny.aguilar@gmail.com'
+sender_pass = 'a1s2d3F$G%'
+receiver_address = 'vluedevil@yahoo.com'
+#Setup the MIME
+message = MIMEMultipart()
+message['From'] = sender_address
+message['To'] = receiver_address
+message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
+#The body and the attachments for the mail
+message.attach(MIMEText(mail_content, 'plain'))
+#Create SMTP session for sending the mail
+session = smtplib.SMTP('smtp.gmail.com', 465) #use gmail with port
+session.starttls() #enable security
+session.login(sender_address, sender_pass) #login with mail_id and password
+text = message.as_string()
+session.sendmail(sender_address, receiver_address, text)
+session.quit()
 # print('Mail Sent')
 
 
--- pdf_docs.py
+++ pdf_docs.py
@@ -11,8 +11,11 @@
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
 
-
+# table data
 fruit = {
     "elderberries": 1,
     "figs": 1,
@@ -22,9 +25,7 @@ fruit = {
     "cherries": 8,
     "grapes": 13
 }
-
 table_data = []
-
 for k, v in fruit.items():
     table_data.append([k, v])
 
@@ -34,11 +35,25 @@ report = SimpleDocTemplate('report.pdf')
 # get styles
 styles = getSampleStyleSheet()
 
-# report title
+# add report title
 report_title = Paragraph('A Complete Inventory of My Fruit', styles['h1'])
 
-# report table data
-report_table = Table(data=table_data)
+# add table style
+table_style = [('GRID', (0, 0), (-1, -1), 1, colors.black)]
+
+# add pie graph
+report_pie = Pie(width=3, height=3)
+report_pie.data = []
+report_pie.labels = []
+for fruit_name in sorted(fruit):
+    report_pie.data.append(fruit[fruit_name])
+    report_pie.labels.append(fruit_name)
+
+report_chart = Drawing()
+report_chart.add(report_pie)
+
+# add report table data
+report_table = Table(data=table_data, style=table_style, hAlign='LEFT')
 
 # build report
-report.build([report_title])
+report.build([report_title, report_table, report_chart])
